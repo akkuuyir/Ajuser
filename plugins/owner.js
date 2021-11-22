fb2aae7e69827cb88a2c54ac103749359d5d0f28
akkuuyir <94748244+akkuuyir@users.noreply.github.com>
2021-11-22 16:11:49 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,16 +6,16 @@ if (Config.WORKTYPE == 'private') {
 
     Julie.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '919745310120-1634557305@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*AJUSER created by aju*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Rafhan Ser created by Rs Bot*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n AJUSER created by aju', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n Rafhan Ser created by Rs Bot', MessageType.text);
         }
     }));
 }
@@ -24,16 +24,16 @@ else if (Config.WORKTYPE == 'public') {
 
     Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '919745310120-1634557305@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*AJUSER created by aju*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Rafhan SER created by Rs Bot*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n AJUSER created by aju', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n Rafhan created by Rs Bot', MessageType.text);
         }
     }));
 }
